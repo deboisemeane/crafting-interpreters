e2f8a3ab77981173835b1430a6350dbcdc2cbd5d
Alex Cook <alexander.cook@healthwords.ai>
2026-02-06 23:14:13 +0000

    feat: insertAtEnd for doubly linked list

--- linked_list.c
+++ linked_list.c
@@ -32,6 +32,21 @@ void insertAtBeginning(Node** head, int data) //Double pointer is used here sinc
     *head = newNode;
 }
 
+void insertAtEnd(Node** head, int data)
+{
+    Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    Node* temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    newNode->prev = temp;
+}
+
 int main() {
     return 0;    
 }
